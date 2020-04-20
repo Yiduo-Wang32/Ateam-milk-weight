06bd0c58a6651f82885266743fb022939f65e17a
Kexin Chen <kchen264@wisc.edu>
2020-04-20 06:52:23 -0500

    Kexin add new class, edit main

--- application/Main.java
+++ application/Main.java
@@ -1,17 +1,27 @@
 package application;
 
+import java.util.List;
 import data.*;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	public static void main(String[] args) {
-		launch(args);
+	
+	private List<String> args;
 
-	}
-
+	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		FarmDataWithYearMonth display = new FarmDataWithYearMonth("66");
+		display.ShowFarmDataTable();
 
 	}
+
+	/**
+	   * @param args
+	   */
+	  public static void main1(String[] args) {
+	    launch(args);
+	  }
 }
