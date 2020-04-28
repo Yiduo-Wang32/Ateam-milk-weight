901a867982af713723a009a34ffa69886cf601a7
Yiduo Wang <ywang2292@wisc.edu>
2020-04-28 14:31:59 -0500

    Commented

--- application/Date.java
+++ application/Date.java
@@ -2,7 +2,9 @@ package application;
 
 /**
  * 
- * Date - TODO Describe purpose of this user-defined type
+ * Date - A class to store a date value. -1 in any of the field represents the
+ * information that is not given
+ * 
  * @author Yiduo Wang (2020)
  *
  */
@@ -11,89 +13,147 @@ public class Date implements Comparable<Date> {
 	private int month;
 	private int day;
 
+	/**
+	 * create Date class with given year, month, day
+	 * 
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
 	public Date(int year, int month, int day) {
 		setYear(year);
 		setMonth(month);
 		setDay(day);
 	}
 
+	/**
+	 * No argument constructor
+	 */
 	public Date() {
 		this(-1, -1, -1);
 	}
 
+	/**
+	 * get the year
+	 * 
+	 * @return the year
+	 */
 	public int getYear() {
 		return this.year;
 	}
 
+	/**
+	 * set the year to given value
+	 * 
+	 * @param year the year to set
+	 * @return true if value is valid and set to year, false otherwise
+	 */
 	public boolean setYear(int year) {
 		this.year = year;
 		return true;
 	}
 
+	/**
+	 * get the month
+	 * 
+	 * @return the month
+	 */
 	public int getMonth() {
 		return this.month;
 	}
 
+	/**
+	 * set the month with the value given
+	 * 
+	 * @param month the month to set, must be an int between 1 to 12, or -1
+	 * @return true if the value given is valid and set to month, false otherwise
+	 */
 	public boolean setMonth(int month) {
 		if (month > 0 && month <= 12) {
 			this.month = month;
 			return true;
-		} else return false;
+		} else
+			return false;
 	}
 
+	/**
+	 * get the day
+	 * 
+	 * @return the day
+	 */
 	public int getDay() {
 		return this.day;
 	}
 
+	/**
+	 * set the day with given value
+	 * 
+	 * @param day the given day. Must be a integer with a range that is valid for
+	 *            putting into the calendar, or -1
+	 * @return true if the value is valid and set, false otherwise
+	 */
 	public boolean setDay(int day) {
-		if (day == -1) {
+		if (day == -1) { // set and return true if day is -1
 			this.day = -1;
 			return true;
-		} else if (day <= 0) {
+		} else if (day <= 0) { // return false if day <= 0 (except -1)
 			return false;
-		} else {
+		} else { // determine the upper limit according to the month
 			switch (month) {
-			case -1:
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:
-				if (day <= 31) {
-					this.day = day;
-					return true;
-				} else return false;
+				// for months with 31 days
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+				case 8:
+				case 10:
+				case 12:
+					if (day <= 31) {
+						this.day = day;
+						return true;
+					} else
+						return false;
 
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if (day <= 30) {
-					this.day = day;
-					return true;
-				} else return false;
+					// for months with 30 days
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					if (day <= 30) {
+						this.day = day;
+						return true;
+					} else
+						return false;
+					// for February, determine whether the year is leap year
+				case 2:
+					if (day <= (isLeapYear() ? 29 : 28)) {
+						this.day = day;
+						return true;
+					} else
+						return false;
 
-			case 2:
-				if (day <= (isLeapYear() ? 29 : 28)) {
-					this.day = day;
-					return true;
-				} else return false;
-
-			default:
-				return false;
+				default:
+					return false;
 			}
 		}
 	}
 
+	/**
+	 * private helper method for determining whether the year is a leap year
+	 */
 	private boolean isLeapYear() {
 		// determine if year is leap year and return the corresponding value
-		if (year % 4 != 0) return false;
-		if (year % 100 == 0 && year % 400 != 0) return false;
-		else return true;
+		if (year % 4 != 0)
+			return false;
+		if (year % 100 == 0 && year % 400 != 0)
+			return false;
+		else
+			return true;
 	}
 
+	/**
+	 * compare with another Date object
+	 */
 	@Override
 	public int compareTo(Date o) {
 		return this.year != o.getYear() ? this.year - o.getYear()
@@ -102,7 +162,7 @@ public class Date implements Comparable<Date> {
 	}
 
 	/**
-	 * convert data to the string with the format month/day/year
+	 * convert data to the string with the format year-month-day
 	 */
 	@Override
 	public String toString() {
--- application/FileManager.java
+++ application/FileManager.java
@@ -43,25 +43,48 @@ import java.util.stream.Stream;
  *
  */
 public class FileManager {
-	public String inputFile;
-	public String outputFile;
+	public String inputFile; // path for input file
+	public String outputFile; // path for output file
 
+	/**
+	 * Set up a FileManager class with the path for input and output file
+	 * 
+	 * @param inputFile  - path for input file
+	 * @param outputFile - path for output file
+	 */
 	public FileManager(String inputFile, String outputFile) {
 		this.inputFile = inputFile;
 		this.outputFile = outputFile;
 	}
 
+	/**
+	 * Constructor for only setting up with input file
+	 * 
+	 * @param inputFile - path for input file
+	 */
 	public FileManager(String inputFile) {
 		this(inputFile, null);
 	}
 
+	/**
+	 * No-argument constructor
+	 */
 	public FileManager() {
 		this(null, null);
 	}
 
+	/**
+	 * Read the input file and store the data into a tree map in the correct format.
+	 * Line with wrongly formatted data will be ignored
+	 * 
+	 * @param farms - the tree map to write in
+	 * @return - true if file is successfully read, false otherwise
+	 */
 	public boolean readFile(TreeMap<String, Farm> farms) {
+		// use a stream to read the file line by line
 		try (Stream<String> lineStream = Files.lines(Paths.get(inputFile))) {
 			lineStream.map(line -> line.split(",")).filter(arr -> arr.length == 3).forEach(arr -> {
+				// store the formatted data to the tree
 				Farm curFarm = farms.get(arr[1]);
 				if (curFarm == null) {
 					curFarm = new Farm();
@@ -71,52 +94,71 @@ public class FileManager {
 			});
 			return true;
 		} catch (IOException e) {
+			// prompt if file name is invalid and return false
 			System.out.println("Invalid input file name");
 			e.printStackTrace();
 			return false;
 		}
 	}
 
+	/**
+	 * Write the data from a given treeMap to the output file, in csv format
+	 * 
+	 * @param farms - treemap that stores the data
+	 * @return - true if the data is successfully written into the file, false
+	 *         otherwise
+	 */
 	public boolean writeToFile(TreeMap<String, Farm> farms) {
 		try (PrintWriter writer = new PrintWriter(new File(outputFile))) {
-			writer.println("date,farm_id,weight");
-			farms.entrySet().stream().map(entry -> entry.getValue())
-					.forEach(farm -> {
-						String name = farm.getId();
-						farm.getData().entrySet().stream().map(entry -> String.format("%s,%s,%d",
-								entry.getKey().toString(), name, entry.getValue()))
-								.forEach(str -> writer.println(str));
-					});
-			
-		} catch (IOException e) {
+			writer.println("date,farm_id,weight"); // write header
+
+			// use a stream to get data stored in the tree
+			farms.entrySet().stream().map(entry -> entry.getValue()).forEach(farm -> {
+				String name = farm.getId();
+				farm.getData().entrySet().stream()
+						// format them into string for output
+						.map(entry -> String.format("%s,%s,%d", entry.getKey().toString(), name,
+								entry.getValue()))
+						// write the string into output file
+						.forEach(str -> writer.println(str));
+			});
+			return true;
+		} catch (IOException e) { // prompt if file name is invalid and return false
 			System.out.println("Invalid output file name");
 			e.printStackTrace();
 			return false;
 		}
-		return true;
+
 	}
 
+	/**
+	 * write data from a given arrayList to the output file, in csv format
+	 * 
+	 * @param - farms ArrayList for storing the data
+	 * @return - true if data is successfully written into the output file, false
+	 *         otherwise
+	 */
 	public boolean writeToFile(ArrayList<TreeMap<String, Data>> farms) {
 		try (PrintWriter writer = new PrintWriter(outputFile)) {
-			writer.println("date,farm_id,weight");
+			writer.println("date,farm_id,weight"); // print header
 
+			// setup a stream to process the data stored in array list
 			farms.stream().forEach(tree -> {
+				// setup another stream for processing the in the treeMap
 				tree.entrySet().stream()
+						// format data into string for output
 						.map(entry -> String.format("%s,%s,%s",
 								entry.getValue().getDate().toString(), entry.getKey(),
 								entry.getValue().getWeight()))
+						// write the string into the file
 						.forEach(str -> writer.println(str));
 			});
-
-		} catch (FileNotFoundException e) {
+			return true;
+		} catch (FileNotFoundException e) { // prompt if file name is invalid and return false
 			System.out.println("Invalid output file name");
 			e.printStackTrace();
 			return false;
 		}
-		return false;
 	}
 
-	public String getFileContents() {
-		return null;
-	}
 }
