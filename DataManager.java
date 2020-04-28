1be470c497ec0f3f4f4df7be9cf492d540a24997
Yumeng Bai <bai54@wisc.edu>
2020-04-28 00:17:01 -0500

    DataManager, all method finished

--- DataManager.java
+++ DataManager.java
@@ -1,8 +1,8 @@
-import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeMap;
 
 public class DataManager {
 
@@ -12,9 +12,9 @@ public class DataManager {
 		this.factory = factory;
 	}
 
-	private HashMap<String, Farm> findDataOfMonth(Date date) {
-		HashMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
-		HashMap<String, Farm> milkDataOfTheMonth = new HashMap<String, Farm>();
+	private TreeMap<String, Farm> findDataOfMonth(Date date) {
+		TreeMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
+		TreeMap<String, Farm> milkDataOfTheMonth = new TreeMap<String, Farm>();
 		milkDataFromFarms.forEach((s, f) -> {
 			Set<Entry<Date, Integer>> dataSetOfFarm = f.getData().entrySet();
 			Iterator<Entry<Date, Integer>> itr = dataSetOfFarm.iterator();
@@ -30,26 +30,26 @@ public class DataManager {
 	}
 
 	public Data getMonthlyAverage(Date date) {
-		HashMap<String, Farm> milkDataOfTheMonth = findDataOfMonth(date);
+		TreeMap<String, Farm> milkDataOfTheMonth = findDataOfMonth(date);
 		String avg = getAvg(milkDataOfTheMonth);
 		return new Data(date, avg);
 	}
 
 	public Data getMonthlyMin(Date date) {
-		HashMap<String, Farm> milkDataOfTheMonth = findDataOfMonth(date);
+		TreeMap<String, Farm> milkDataOfTheMonth = findDataOfMonth(date);
 		return getMin(milkDataOfTheMonth);
 
 	}
 
 	public Data getMonthlyMax(Date date) {
-		HashMap<String, Farm> milkDataOfTheMonth = findDataOfMonth(date);
+		TreeMap<String, Farm> milkDataOfTheMonth = findDataOfMonth(date);
 		return getMin(milkDataOfTheMonth);
 
 	}
 
-	private HashMap<String, Farm> findDataOfFarmOfMont(Date date, String farmID) {
-		HashMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
-		HashMap<String, Farm> milkDataOfTheMonth = new HashMap<String, Farm>();
+	private TreeMap<String, Farm> findDataOfFarmOfMont(Date date, String farmID) {
+		TreeMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
+		TreeMap<String, Farm> milkDataOfTheMonth = new TreeMap<String, Farm>();
 		Farm farm = milkDataFromFarms.get(farmID);
 		Set<Entry<Date, Integer>> dataSetOfFarm = farm.getData().entrySet();
 		Iterator<Entry<Date, Integer>> itr = dataSetOfFarm.iterator();
@@ -66,28 +66,42 @@ public class DataManager {
 	}
 
 	public Data getMonthlyAverageForFarm(Date date, String farmID) {
-		HashMap<String, Farm> milkDataOfTheMonth = findDataOfFarmOfMont(date, farmID);
+		TreeMap<String, Farm> milkDataOfTheMonth = findDataOfFarmOfMont(date, farmID);
 		return new Data(date, getAvg(milkDataOfTheMonth));
 	}
 
 	public Data getMonthlyMinForFarm(Date date, String farmID) {
-		HashMap<String, Farm> milkDataOfTheMonth = findDataOfFarmOfMont(date, farmID);
+		TreeMap<String, Farm> milkDataOfTheMonth = findDataOfFarmOfMont(date, farmID);
 		return getMin(milkDataOfTheMonth);
 
 	}
 
 	public Data getMonthlyMaxForFarm(Date date, String farmID) {
-		HashMap<String, Farm> milkDataOfTheMonth = findDataOfFarmOfMont(date, farmID);
+		TreeMap<String, Farm> milkDataOfTheMonth = findDataOfFarmOfMont(date, farmID);
 		return getMax(milkDataOfTheMonth);
 	}
 
-	public Data getDataSortedByField() {
-		return null;
+	public ArrayList<TreeMap<String,Data>> getDataSortedByField() {
+		ArrayList<TreeMap<String, Data>> sortedByMonth = new ArrayList<TreeMap<String,Data>>();
+		TreeMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
+		Set<Entry<String,Farm>> dataSet = milkDataFromFarms.entrySet();
+		Iterator<Entry<String,Farm>> itr = dataSet.iterator();
+		while (itr.hasNext()) {
+			Entry<String, Farm> curr = itr.next();
+			TreeMap<Date, Integer> data = curr.getValue().getData();
+			Iterator<Entry<Date, Integer>> dataItr = data.entrySet().iterator();
+			while (dataItr.hasNext()) {
+				Entry<Date, Integer> currData = dataItr.next();
+				int index = currData.getKey().getMonth() -1;
+				sortedByMonth.get(index).put(curr.getKey(), new Data(currData.getKey(),currData.getValue().toString()));
+			}
+		}
+		return sortedByMonth;
 	}
 
-	private HashMap<String, Farm> getDataInDateRange(Date d1, Date d2) {
-		HashMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
-		HashMap<String, Farm> milkDataInRange = new HashMap<String, Farm>();
+	private TreeMap<String, Farm> getDataInDateRange(Date d1, Date d2) {
+		TreeMap<String, Farm> milkDataFromFarms = factory.getMilkDataFromFarms();
+		TreeMap<String, Farm> milkDataInRange = new TreeMap<String, Farm>();
 		milkDataFromFarms.forEach((s, f) -> {
 			Set<Entry<Date, Integer>> dataSetOfFarm = f.getData().entrySet();
 			Iterator<Entry<Date, Integer>> itr = dataSetOfFarm.iterator();
@@ -105,68 +119,28 @@ public class DataManager {
 	}
 
 	public Data getAverageInDateRange(Date d1, Date d2) {
-		HashMap<String, Farm> milkDataInRange = getDataInDateRange(d1, d2);
+		TreeMap<String, Farm> milkDataInRange = getDataInDateRange(d1, d2);
 		return new Data(d1, getAvg(milkDataInRange));
 	}
 
 	private boolean inDateRange(Date day, Date dayMin, Date dayMax) {
-		int dayYear = day.getYear();
-		int dayMonth = day.getMonth();
-		int dayDay = day.getDay();
-		int minYear = dayMin.getYear();
-		int minMonth = dayMin.getMonth();
-		int minDay = dayMin.getDay();
-		int maxYear = dayMax.getYear();
-		int maxMonth = dayMax.getMonth();
-		int maxDay = dayMax.getDay();
-
-		if (dayYear > minYear && dayYear < maxYear) {
+		if(day.compareTo(dayMin) >= 0 && day.compareTo(dayMax) <= 0) {
 			return true;
-		} else if (dayYear < minYear || dayYear > maxYear) {
-			return false;
-		} else if (dayYear == minYear) {
-			if (dayMonth > minMonth) {
-				return true;
-			} else if (dayMonth < minMonth) {
-				return false;
-			} else {
-				// dayMonth = minMonth
-				if (dayDay >= minDay) {
-					return true;
-				} else {
-					return false;
-				}
-			}
-		} else {
-			// dayYear = maxYear
-			if (dayMonth < maxMonth) {
-				return true;
-			} else if (dayMonth > maxMonth) {
-				return false;
-			} else {
-				// dayMonth = maxMonth
-				if (dayDay <= maxDay) {
-					return true;
-				} else {
-					return false;
-				}
-			}
-
 		}
-
+		return false;
 	}
 
 	public Data getMinInDateRange(Date d1, Date d2) {
-		HashMap<String, Farm> milkDataInRange = getDataInDateRange(d1, d2);
+		TreeMap<String, Farm> milkDataInRange = getDataInDateRange(d1, d2);
 		return getMin(milkDataInRange);
 	}
 
 	public Data getMaxInDateRange(Date d1, Date d2) {
-		HashMap<String, Farm> milkDataInRange = getDataInDateRange(d1, d2);
+		TreeMap<String, Farm> milkDataInRange = getDataInDateRange(d1, d2);
 		return getMax(milkDataInRange);
 	}
 
-	private String getAvg(HashMap<String, Farm> dataMap) {
+	private String getAvg(TreeMap<String, Farm> dataMap) {
 		Set<Entry<String, Farm>> dataSet = dataMap.entrySet();
 		double tally = 0.0;
 		long sum = 0;
@@ -186,7 +160,7 @@ public class DataManager {
 		return Double.toString(avg);
 	}
 
-	private Data getMin(HashMap<String, Farm> dataMap) {
+	private Data getMin(TreeMap<String, Farm> dataMap) {
 		Set<Entry<String, Farm>> dataSet = dataMap.entrySet();
 		Data minData = new Data(null, "");
 		int minWeight = 0;
@@ -212,7 +186,7 @@ public class DataManager {
 		return minData;
 	}
 
-	private Data getMax(HashMap<String, Farm> dataMap) {
+	private Data getMax(TreeMap<String, Farm> dataMap) {
 		Set<Entry<String, Farm>> dataSet = dataMap.entrySet();
 		Data maxData = new Data(null, "");
 		int maxWeight = 0;
